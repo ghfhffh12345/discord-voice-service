a6d059575a01beb798803fe7bfe43882edaf9363
cruelcat <79185132+ghfhffh12345@users.noreply.github.com>
2026-05-18 13:02:02 +0900

    test: assert full discord rtp packet

--- tests/discord_voice_gateway.rs
+++ tests/discord_voice_gateway.rs
@@ -3,7 +3,10 @@ use discord_voice_service::discord_voice::rtp::RtpPacketBuilder;
 #[test]
 fn builds_rtp_header_for_discord_voice() {
     let packet = RtpPacketBuilder::new(7).build(1, 960, &[0xAA, 0xBB]);
-    assert_eq!(packet[0], 0x80);
-    assert_eq!(packet[1], 0x78);
-    assert_eq!(&packet[2..4], &[0x00, 0x01]);
+    assert_eq!(
+        packet,
+        vec![
+            0x80, 0x78, 0x00, 0x01, 0x00, 0x00, 0x03, 0xC0, 0x00, 0x00, 0x00, 0x07, 0xAA, 0xBB,
+        ]
+    );
 }
