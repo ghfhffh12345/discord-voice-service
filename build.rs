22f42d2e6a3adfb6e2ef63c0298acf8e564bc927
cruelcat <79185132+ghfhffh12345@users.noreply.github.com>
2026-05-18 11:10:22 +0900

    feat: define control service protobuf contract

--- build.rs
+++ build.rs
@@ -1,1 +1,12 @@
-fn main() {}
+fn main() {
+    let protoc = protoc_bin_vendored::protoc_bin_path().expect("protoc");
+    unsafe {
+        std::env::set_var("PROTOC", protoc);
+    }
+
+    tonic_build::configure()
+        .build_server(true)
+        .build_client(true)
+        .compile_protos(&["proto/discordvoice/v1/control.proto"], &["proto"])
+        .expect("compile protos");
+}
